84678552e5a094f560b7ed1ad380089e8b51be23
tsiannis <61150021+tsiannisk@users.noreply.github.com>
2025-03-28 20:13:14 +0200

    Add files via upload

---
+++ dataset.py
@@ -0,0 +1,21 @@
+import lyricsgenius
+genius = lyricsgenius.Genius("tzz1gzLd2XN0FDzQjmwPnXZYi8P9bVLVnXPsznqLyyzyzYH04TPwZaPGuUB30ZiU")
+
+
+def save_song(song_artist: str, song_name: str, lyrics: str):
+
+    # Create a valid filename
+    filename = "songs.txt"
+
+    # Save the lyrics to the file
+    with open(filename, "a", encoding="utf-8") as file:
+        file.write(song_name + " by " + song_artist + "\n\n")
+        file.write(lyrics + "\n\n")
+
+
+artist = genius.search_artist("Snik",max_songs = 40, sort="title")
+print(artist.songs)
+
+for a in artist.songs:
+    songtemp = artist.song(a.title)
+    save_song(a.artist, a.title, artist.song(a.title).lyrics)
